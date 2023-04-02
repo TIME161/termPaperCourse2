464c0fa19aa742e3686587a057a210a5228c88af
TIME <opout777@yandex.ru>
2023-04-02 13:11:33 +0300

    First commit

---
+++ src/test/java/pro/sky/termPaperCourse2/ExamGiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.termPaperCourse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
